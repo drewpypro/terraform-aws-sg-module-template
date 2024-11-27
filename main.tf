551e0dc2136737882c36c50b2adf4301f10c5a4f
drewpypro <83561525+drewpypro@users.noreply.github.com>
2024-11-27 10:55:17 -0700

    test

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
-variable "security_group_files" {
-  default = fileset("${path.module}/security_groups", "*.json")
+# Use a local block for dynamic fileset loading
+locals {
+  security_group_files = fileset("${path.module}/security_groups", "*.json")
 }
 
 # Loop through JSON files to create security groups
 resource "aws_security_group" "security_groups" {
-  for_each = { for file in var.security_group_files : file => jsondecode(file("${path.module}/security_groups/${file}")) }
+  for_each = { for file in local.security_group_files : file => jsondecode(file("${path.module}/security_groups/${file}")) }
 
   name        = each.value.aws_security_group
   description = "Security group for ${each.key}"
@@ -14,7 +15,7 @@ resource "aws_security_group" "security_groups" {
 # Loop through rules in each security group and create them
 resource "aws_security_group_rule" "rules" {
   for_each = flatten([
-    for sg_file in var.security_group_files : [
+    for sg_file in local.security_group_files : [
       for rule in jsondecode(file("${path.module}/security_groups/${sg_file}")).rules : {
         sg_id = aws_security_group.security_groups[sg_file].id
         rule  = rule
--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "services" {
   description = "List of services requiring VPC endpoint security groups"
   type        = list(string)
-  default     = ["ec2", "ssm"]
+  default     = ["ec2", "ssm", "kms", "sts"]
 }
 
 variable "referenced_sg_ids" {
@@ -45,4 +45,16 @@ variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
   default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "external_endpoint" {
+  description = "CIDR blocks for external endpoint"
+  type        = list(string)
+  default     = ["69.69.69.69/32"]
+}
+
+variable "private_networks" {
+  description = "CIDR blocks for private networks"
+  type        = list(string)
+  default     = ["10.10.69.0/24", "10.69.69.0/24"]
 }
