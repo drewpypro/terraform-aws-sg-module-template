1010f8ee4375a701e0bbccbf07e65e51480fdacd
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-01-03 14:29:39 -0700

    updated keys

--- providers.tf
+++ providers.tf
@@ -6,12 +6,4 @@ terraform {
     }
   }
 
-  backend "s3" {
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    skip_s3_checksum            = true
-    region                      = "us-east-1"
-  }
 }
